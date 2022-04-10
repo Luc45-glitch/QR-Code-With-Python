9e1d92aad1eaba3f3e16f3e0718b4c309efa1b85
45-glitch <85082830+Luc45-glitch@users.noreply.github.com>
2022-04-09 21:46:54 -0400

    Update QR.py

--- QR.py
+++ QR.py
@@ -1,7 +1,9 @@
 import qrcode
+#aca deberas poner la url que deses que se convierta en QR
 url="https://google.com"
 qr=qrcode.QRCode(version=1,box_size=10,border=5)
 qr.add_data(url)
 qr.make(fit=True)
 img=qr.make_image(fill='black',back_color='white')
+#Y te recomiendo aca vas cambiando el numerito de la imagen
 img.save('qrcode001.png')
